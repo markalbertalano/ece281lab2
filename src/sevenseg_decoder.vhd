92436c7fe85c97032c6be760dc81a31fe47d7f34
markalbertalano <markalbertalano@users.noreply.github.com>
2025-02-25 16:41:32 -0700

    lab2.setup.2

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -38,7 +38,25 @@ end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
 
-begin
-
+begin --g left a right
+    
+    with i_Hex select
+    o_seg_n <= "0111111" when "0000",--0
+               "0000110" when "0001",
+               "1011011" when "0010",
+               "1001111" when "0011",
+               "1100110" when "0100",
+               "0000110" when "0101",--5
+               "1111101" when "0110",
+               "0000111" when "0111",
+               "1111111" when "1000",
+               "1100111" when "1001",--9
+               "1110111" when "1010",--A
+               "1111100" when "1011",--B
+               "1011000" when "1100",--c
+               "1011110" when "1101",--d
+               "1111001" when "1110",--E
+               "1110001" when "1111",--F
+               "0000000" when others;
 
 end Behavioral;
